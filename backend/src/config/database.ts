b1fcc4b3912514255c2d335b929f521d5d737b00
WallefEduardo <wallefeduardo18@gmail.com>
2025-06-26 22:06:29 -0300

    Atualizaçção de Logos

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -24,11 +24,11 @@ module.exports = {
     max: 100
   },
   pool: {
-    max: parseInt(process.env.DB_POOL_MAX) || 100,
-    min: parseInt(process.env.DB_POOL_MIN) || 15,
-    acquire: parseInt(process.env.DB_POOL_ACQUIRE) || 30000,
-    idle: parseInt(process.env.DB_POOL_IDLE) || 600000
-  },
+  max: parseInt(process.env.DB_POOL_MAX) || 15,
+  min: parseInt(process.env.DB_POOL_MIN) || 0,
+  acquire: parseInt(process.env.DB_POOL_ACQUIRE) || 30000,
+  idle: parseInt(process.env.DB_POOL_IDLE) || 10000
+    },
   dialect: process.env.DB_DIALECT || "postgres",
   timezone: 'America/Sao_Paulo',
   host: process.env.DB_HOST || "localhost",
